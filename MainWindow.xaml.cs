8150adf210b29b1638310846c2a91b39fd30e380
FNU LNU <qxbs@qxbsupc.onmicrosoft.com>
2024-08-17 14:27:53 +0800

    add files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Policy;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string savePth = @"\\SlayTheSpire\\saves";
+        const string savebackupPth = "backups";
+        string gamePth = "";
+        readonly string currentPth =  Directory.GetCurrentDirectory();
+        //readonly string basefilename = DateTime.Now.ToString("yyyyMMddhhmm");
+
+        CancellationTokenSource source;
+        CancellationToken token;
+        //Task thread;
+        
+        public MainWindow()
+        {
+            source = new CancellationTokenSource();
+            token = source.Token;
+            //thread = new Thread(() => listenSaves(token,gamePth, System.IO.Path.Combine(currentPth, savebackupPth)));
+            InitializeComponent();
+            LoadOrCreateConfigFile();
+            if (!Directory.Exists(System.IO.Path.Combine(currentPth, savebackupPth)))
+            {
+                // 文件夹不存在，创建新的文件夹
+                Directory.CreateDirectory(System.IO.Path.Combine(currentPth, savebackupPth));
+            }
+            string[] Jobs = ["IRONCLAD", "THE_SILENT", "DEFECT", "WATCHER"];
+            for (int i = 0; i < 4; i++)
+            {
+                string filePth = System.IO.Path.Combine(System.IO.Path.Combine(currentPth, savebackupPth), Jobs[i]);
+                if (!Directory.Exists(filePth))
+                {
+                    Directory.CreateDirectory(filePth);
+                }
+            }
+        }
+        private void LoadOrCreateConfigFile()
+        {
+            string directoryPath = AppDomain.CurrentDomain.BaseDirectory; // 获取应用程序的基目录
+            string configFilePath = System.IO.Path.Combine(directoryPath, "config.txt"); // 配置文件路径
+
+            if (File.Exists(configFilePath))
+            {
+                string configContent = File.ReadAllText(configFilePath);
+                Console.WriteLine("配置文件" + configContent);
+                if (Regex.IsMatch(configContent, savePth))
+                {
+                    gamePth = configContent;
+                    Console.WriteLine("配置的目录有效：" + configContent);
+                }
+                else
+                {
+                    SelectAndSaveDirectory(configFilePath);
+                }
+            }
+            else
+            {
+                SelectAndSaveDirectory(configFilePath);
+                string configContent = File.ReadAllText(configFilePath);
+                gamePth = configContent;
+            }
+        }
+        private static void SelectAndSaveDirectory(string configFilePath)
+        {
+            Microsoft.Win32.OpenFolderDialog dialog = new();
+            dialog.Multiselect = false;
+            dialog.Title = "选择SlayTheSpire/saves目录";
+
+            bool? result = false;
+            while (result != true)
+            {
+                result = dialog.ShowDialog();
+                if (result == false)
+                {
+                    Environment.Exit(0);
+                }
+                else if (!Regex.IsMatch(dialog.FolderName, savePth))
+                {
+                    MessageBox.Show("请选择SlayTheSpire/saves目录。");
+                    result = false;
+                }
+            }
+            string fullPathToFolder = dialog.FolderName;
+            try
+            {
+                using (StreamWriter sw = File.CreateText(configFilePath))
+                {
+                    sw.Write(fullPathToFolder);
+                }
+                Console.WriteLine("配置文件已创建。");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("写入配置文件时出错: " + ex.Message);
+            }
+        }
+        private void btn1Click(object sender, RoutedEventArgs e)
+        {
+            Button? btn1 = sender as Button;
+            if (btn1 != null)
+            {
+                if (btn1.Content.ToString() == "开始备份存档")
+                {
+                    
+                    btn1.Content = "停止备份存档";
+                    source = new CancellationTokenSource();
+                    token = source.Token;
+
+                    Task mytask = Task.Run(() => listenSaves(token, gamePth, System.IO.Path.Combine(currentPth, savebackupPth)),token);
+
+                    // 启动线程
+                    //thread.Start();
+                    
+                    MessageBox.Show("开始备份！");
+                }
+                else
+                {
+                    btn1.Content = "开始备份存档";
+                    MessageBox.Show("已停止！");
+                    source.Cancel();
+                }   
+            }
+            else
+                MessageBox.Show("按钮为空！");
+        }
+        private void btn2Click(object sender, RoutedEventArgs e)
+        {
+            if (!token.IsCancellationRequested)
+                source.Cancel();
+            Button btn1 = (Button)FindName("btn1");
+            if(btn1.Content.ToString() == "停止备份存档")
+            {
+                MessageBox.Show("已停止！");
+                btn1.Content = "开始备份存档";
+            }
+            
+            this.Visibility = Visibility.Hidden;
+            Window1 window1 = new Window1();
+            window1.Closed += (s, args) =>
+            {
+                this.Visibility = Visibility.Visible;
+            };
+            window1.Show();
+        }
+        private static void listenSaves(CancellationToken token,string gamesavePth,string bkPth)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine("线程正在执行。");
+                //gamesavePth = gamesavePth.TrimEnd(System.IO.Path.DirectorySeparatorChar);
+                string[] result = Directory.GetFiles(gamesavePth, "*.autosave");
+                //接下来这里每5s读取一次存档，与上次进行比较，不同就保存新的
+                if (result.Length<=0)
+                {
+                    Console.WriteLine("游戏还没有存档");
+                    Thread.Sleep(100);
+                    continue;
+                }
+                string sourcePth = result[0];
+                //MessageBox.Show("发现存档！" + sourcePth);
+                string desthead = System.IO.Path.GetFileNameWithoutExtension(sourcePth);
+                string destionPth = System.IO.Path.Combine(System.IO.Path.Combine(bkPth,desthead), DateTime.Now.ToString("yyyyMMddHHmmss") + ".save");
+                string hash0 = hashFile(sourcePth);
+                
+                if (!DirectoryHasFiles(System.IO.Path.Combine(bkPth, desthead)))
+                {
+                    try
+                    {
+                        // 复制文件到目标目录，并使用当前时间戳作为文件名
+                        File.Copy(sourcePth, destionPth);
+                        Console.WriteLine($"文件已成功复制到 '{destionPth}'");
+                        //MessageBox.Show($"文件已成功复制到 '{destionPth}'");
+                        //MessageBox.Show("复制文件完成！ " );
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("复制文件时发生错误: " + ex.Message);
+                        //MessageBox.Show("复制文件时发生错误: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    FileInfo latestFile = FindLatestFileByNameTimestamp(System.IO.Path.Combine(bkPth, desthead));
+                    if (latestFile != null)
+                    {
+                        string hash2 = hashFile(latestFile.FullName);
+                        //MessageBox.Show("hash0: " + hash0);
+                        //MessageBox.Show("hash2: " + hash2);
+                        if (hash0 != hash2)
+                        {
+                            //MessageBox.Show("newest file: " + latestFile.FullName);
+                            //MessageBox.Show("hash 不相等");
+                            destionPth = System.IO.Path.Combine(System.IO.Path.Combine(bkPth, desthead), DateTime.Now.ToString("yyyyMMddHHmmss") + ".save");
+                            try
+                            {
+                                // 复制文件到目标目录，并使用当前时间戳作为文件名
+                                File.Copy(sourcePth, destionPth);
+                                Console.WriteLine($"文件已成功复制到 '{destionPth}'");
+                                //MessageBox.Show($"文件已成功复制到 '{destionPth}'");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("复制文件时发生错误: " + ex.Message);
+                                //MessageBox.Show("复制文件时发生错误: " + ex.Message);
+                            }
+                            
+                        }
+                        //else
+                        //{
+                        //    MessageBox.Show("存档没有变化！");
+                        //}
+                    }
+                }
+
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("线程已取消。");
+        }
+        static bool DirectoryHasFiles(string directoryPath)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+            return directoryInfo.GetFiles().Length > 0;
+        }
+
+        static FileInfo FindLatestFileByNameTimestamp(string directoryPath)
+        {
+            var di = new DirectoryInfo(directoryPath);//文件夹所在目录
+            FileInfo[] fileList = di.GetFiles();
+            if (fileList.Length > 0)
+            {
+                Array.Sort(fileList, new Comparison<FileInfo>((x, y) => x.CreationTime.CompareTo(y.CreationTime)));
+                return fileList[fileList.Length - 1];
+            }
+            return null;
+        }
+        // 计算文件的哈希值
+        static string hashFile(string filePath)
+        {
+            //using (HashAlgorithm hash = HashAlgorithm.Create())
+            //{
+            //    using (FileStream file1 = new FileStream(filePath, FileMode.Open))
+            //    {
+            //        byte[] hashByte1 = hash.ComputeHash(file1);
+                    
+            //    }
+            //}
+            using (var sha256 = SHA256.Create())
+            {
+                using (FileStream file1 = new FileStream(filePath, FileMode.Open))
+                {
+                    byte[] hash = sha256.ComputeHash(file1);
+                    return BitConverter.ToString(hash).Replace("-", String.Empty);
+
+                }
+                
+            }
+        }
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://blog.csdn.net/chouzhou9701/article/details/121235873";
+            try
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                // 处理可能发生的异常
+                MessageBox.Show($"无法打开链接：{ex.Message}");
+            }
+        }
+        private void Hyperlink_Click2(object sender, RoutedEventArgs e)
+        {
+            string url = "https://github.com/qbn-ing/Slay-the-Spire-Save-File-Manager/";
+            try
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                // 处理可能发生的异常
+                MessageBox.Show($"无法打开链接：{ex.Message}");
+            }
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            source.Cancel();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using System.Runtime.InteropServices.JavaScript;
+using System.Diagnostics;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        readonly string currentPth = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "backups");
+        readonly string configFilePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "config.txt");
+        public Window1()
+        {
+            InitializeComponent();
+            getallSaves();
+            savesdt.AutoGenerateColumns = false;
+            //SaveData sd = new SaveData("D:\\Software\\Steam\\steamapps\\common\\SlayTheSpire\\saves\\IRONCLAD.autosave");
+        }
+        //这里要解析保存的存档，简略信息，点击可以显示存档详细信息，来让用户选择
+        public void getallSaves()
+        {
+            List<SaveData> saveDataList = [];
+            try
+            {
+                string[] Job = ["IRONCLAD", "THE_SILENT", "DEFECT", "WATCHER"];
+                for (int i = 0; i < 4; i++)
+                {
+                    string filePth = System.IO.Path.Combine(currentPth, Job[i]);
+                    if (Directory.Exists(filePth))
+                    {
+                        string[] files = Directory.GetFiles(filePth);
+                        foreach (string file in files)
+                        {
+                            SaveData saveData = new SaveData(file, Job[i]);
+                            saveDataList.Add(saveData);
+                        }
+                    }
+                }
+                savesdt.ItemsSource = saveDataList;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        private SaveData selectedData;
+        public void dt_select(object sender, SelectionChangedEventArgs e)
+        {
+            if (savesdt.SelectedItem != null)
+            {
+                selectedData = savesdt.SelectedItem as SaveData;
+            }
+        }
+        public void repleaceSaves(SaveData destSave)
+        {
+            string configContent = File.ReadAllText(configFilePath);
+            string[] files = Directory.GetFiles(configContent,"*.autosave");
+            string targetJob = destSave.Job;
+            try
+            {
+                if (files.Length > 0)
+                {
+                    foreach (string file in files)
+                    {
+                        string name = System.IO.Path.GetFileNameWithoutExtension(file);
+                        if (name == targetJob)
+                        {
+                            File.Delete(file);
+                            File.Copy(destSave.Pth, file);
+                        }
+                        else
+                        {
+                            File.Delete(file);
+                            File.Delete(System.IO.Path.Combine(configContent, name + ".autosave.backUp"));
+                            File.Copy(destSave.Pth, System.IO.Path.Combine(configContent, targetJob + ".autosave"));
+                            File.Copy(destSave.Pth, System.IO.Path.Combine(configContent, targetJob + ".autosave.backUp"));
+                        }
+                    }
+                }
+                else
+                {
+                    File.Copy(destSave.Pth, System.IO.Path.Combine(configContent, targetJob + ".autosave"));
+                    File.Copy(destSave.Pth, System.IO.Path.Combine(configContent, targetJob + ".autosave.backUp"));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("文件正在被使用，请稍后再试！");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public void btn3_click(object sender, RoutedEventArgs e)
+        {
+            if (selectedData != null)
+            {
+                //MessageBox.Show($"Name: {selectedData.Name}\n" +
+                //                $"Level: {selectedData.Level}\n" +
+                //                $"Floor: {selectedData.Floor}\n" +
+                //                $"Gold: {selectedData.Gold}\n" +
+                //                $"Seed: {selectedData.Seed}\n" +
+                //                $"Max Health: {selectedData.MaxHealth}\n" +
+                //                $"Current Health: {selectedData.CurrentHealth}\n" +
+                //                $"Job: {selectedData.Job}\n" +
+                //                $"Date: {selectedData.Date}\n");
+                MessageBoxResult result = MessageBox.Show(
+                                "确定要替换为当前存档吗？此操作不可撤销！！",
+                                "替换存档",
+                                MessageBoxButton.YesNo,
+                                MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    repleaceSaves(selectedData);
+                }
+
+
+            }
+        }
+        public void btn5_click(object sender, RoutedEventArgs e)
+        {
+            if (selectedData != null)
+            {
+                MessageBoxResult result = MessageBox.Show(
+                                "确定要删除当前存档吗？此操作不可撤销！！",
+                                "删除存档",
+                                MessageBoxButton.YesNo,
+                                MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+
+                    File.Delete(selectedData.Pth);
+                    getallSaves();
+                }
+
+
+            }
+        }
+        public void btn4_click(object sender, RoutedEventArgs e)
+        {
+            if (selectedData != null)
+            {
+                if (File.Exists(selectedData.Pth))
+                {
+                    // 获取文件所在的目录路径
+                    string directoryPath = System.IO.Path.GetDirectoryName(selectedData.Pth);
+                    // 打开该目录
+                    Process.Start("explorer.exe", directoryPath);
+                }
+                else
+                {
+                    MessageBox.Show("文件路径无效或文件不存在。");
+                }
+
+            }
+        }
+    }
+
+    public class SaveData
+    {
+        public string Name { get; private set; }
+        public Int32 CurrentHealth { get; private set; }
+        public Int32 MaxHealth { get; private set; }
+        public Int32 Level { get; private set; }
+        public Int32 Floor { get; private set; }
+        public Int32 Gold { get; private set; }
+        public Int64 Seed { get; private set; }
+        public string Job { get; private set; }
+        public string Date { get; private set; }
+        public string Pth { get; private set; }
+        public SaveData(string pth, string job)
+        {
+            //MessageBox.Show(pth);
+            this.Job = job;
+            this.Date = File.GetLastWriteTime(pth).ToString();
+            this.Pth = pth;
+            string base64String = File.ReadAllText(pth);
+            byte[] bytes = Convert.FromBase64String(base64String);
+            byte[] bytes1 = { (byte)'k', (byte)'e', (byte)'y' };
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] ^= bytes1[i % 3];
+            }
+            string saveJson = Encoding.UTF8.GetString(bytes);
+            JObject jsonObj;
+            try
+            {
+                jsonObj = JObject.Parse(saveJson);
+
+                //foreach (var property in jsonObj.Properties())
+                //{
+                //    // 获取键（Key）
+                //    string key = property.Name;
+                //    // 获取值（Value）
+                //    JToken value = property.Value;
+
+                //    // 输出键值对
+                //    //MessageBox.Show($"{key}: {value}");
+                //    Console.WriteLine($"{key}: {value}");
+                //}
+
+                if (jsonObj != null)
+                {
+                    this.Floor = jsonObj.Value<int?>("floor_num") ?? default(int);
+                    this.Seed = jsonObj.Value<long?>("seed") ?? default(int);
+                    this.Gold = jsonObj.Value<int?>("gold") ?? default(int);
+                    this.Level = jsonObj.Value<int?>("ascension_level") ?? default(int);
+                    this.MaxHealth = jsonObj.Value<int?>("max_health") ?? default(int);
+                    this.CurrentHealth = jsonObj.Value<int?>("current_health") ?? default(int);
+                    this.Name = jsonObj.Value<string>("name");
+                    //MessageBox.Show($"Name: {this.Name}\n" +
+                    //                $"Level: {this.Level}\n" +
+                    //                $"Floor: {this.Floor}\n" +
+                    //                $"Gold: {this.Gold}\n" +
+                    //                $"Seed: {this.Seed}\n" +
+                    //                $"Max Health: {this.MaxHealth}\n" +
+                    //                $"Current Health: {this.CurrentHealth}\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
